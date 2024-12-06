d9c56f6d28268aaa809006ccc145daa0e96de1f0
Wendrew Oliveira <developer.wendrew@gmail.com>
2024-12-06 14:50:30 -0300

    Create a basic websocket connection module

--- ehr_library/core.py
+++ ehr_library/core.py
@@ -1,13 +1,36 @@
 import urllib3
 from urllib.parse import urlencode, urlparse, parse_qs, urlunparse
 from http.cookiejar import CookieJar
-from session import HTTPSessionManager
+from .session import HTTPSessionManager
+from .sockets import WebSocketManager
 
 
 class Request:
-    def __init__(self, method):
+    def __init__(self, method, debug=False):
         self.method = method
         self.session_manager = HTTPSessionManager()
+        self.debug = debug
+
+    def _sanitize_headers(self, headers):
+        if not headers:
+            return {}
+
+        sanitized = headers.copy()
+        sensitive_keys = {"Authorization", "Cookie", "Set-Cookie"}
+        for key in sensitive_keys:
+            sanitized[key] = "[REDACTED]"
+
+        return sanitized
+
+    def _log_request(self, method, url, headers, params=None):
+        if self.debug:
+            sanitized_headers = self._sanitize_headers(headers)
+            print("DEBUG: HTTP Request")
+            print(f'Method::: {method}')
+            print(f'Url::: {url}')
+            if params:
+                print(f'Params::: {params}')
+            print(f'Headers::: {sanitized_headers}')
 
     def build_url(self, base_url, params=None):
         """
@@ -35,7 +58,7 @@ class Request:
         ))
 
         return new_url
-    
+
     def request(self, url: str, headers=None, body=None, params=None):
         """
         Send an HTTP request using the specified method.
@@ -48,6 +71,8 @@ class Request:
         """
 
         url = self.build_url(url, params)
+        self._log_request(self.method, url, headers, params)
+
         match self.method.upper():
             case "POST":
                 return self.session_manager.request(
